e51a6c01772ae377b5e3825cbcbfd39360fc246f
Jake Reid <jake@Jakes-MacBook-Pro.local>
2017-04-05 16:45:31 +0100

    words

--- app.js
+++ app.js
@@ -7,8 +7,14 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 5000));
 
-app.get('/facebook', function (req, res){
-    res.send('1983647249');
+app.get("/facebook", function (req, res) {
+  if (req.query["hub.verify_token"] === "this_is_my_token") {
+    console.log("Verified webhook");
+    res.status(200).send(req.query["hub.challenge"]);
+  } else {
+    console.error("Verification failed. The tokens do not match.");
+    res.sendStatus(403);
+  }
 });
 
 app.post('/facebook', function (req, res){
