c01793ccd889258d5890238d49544cde6fba7059
Jake Reid <jake@Jakes-MacBook-Pro.local>
2017-04-05 16:21:46 +0100

    bout to do heroku

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const request = require('request');
 const bodyParser = require('body-parser');
-
+const TOKEN = 'EAAUi56w09j8BAJjZBGcX7qjVJcNeYdIEatq3BGNGKxOL5InZBr340cezl9E1j3XgflHBNZC9HEuGrpZAipmZAxyC1HTUDMxOL2kEZBUlhsAAp8Lg4RtP5hACKg1iDdOZAK7o8ttcVZCB4GCtXIeXGV12iGHMpkFbDIUHApQ9PiA84QZDZD';
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 5000));
 
 app.get('/jake', function (req, res){
-    console.log(res)
     res.send('Deployed!');
 });
 
