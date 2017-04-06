66b09f39a760213eca16b26dee36165de6cf24e6
Jake Reid <jake@Jakes-MacBook-Pro.local>
2017-04-06 09:32:54 +0100

    using good code now

--- app.js
+++ app.js
@@ -17,67 +17,37 @@ app.get("/facebook", function (req, res) {
   }
 });
 
-app.post("/facebook", function (req, res) {
+app.post('/facebook', function (req, res) {
+  var data = req.body;
+
   // Make sure this is a page subscription
-  if (req.body.object == "page") {
-    // Iterate over each entry
-    // There may be multiple entries if batched
-    req.body.entry.forEach(function(entry) {
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
       // Iterate over each messaging event
       entry.messaging.forEach(function(event) {
-        console.log(event);
         if (event.message) {
-          processPostback(event);
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
         }
       });
     });
 
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
     res.sendStatus(200);
   }
 });
-
-function processPostback(event) {
-  var senderId = event.sender.id;
-  var payload = event.message.text;
-
-  if (payload.toLowerCase() === "hi") {
-    // Get user's first name from the User Profile API
-    // and include it in the greeting
-    request({
-      url: "https://graph.facebook.com/v2.6/" + senderId,
-      qs: {
-        access_token: process.env.PAGE_ACCESS_TOKEN,
-        fields: "first_name"
-      },
-      method: "GET"
-    }, function(error, response, body) {
-      var greeting = "";
-      if (error) {
-        console.log("Error getting user's name: " +  error);
-      } else {
-        var bodyObj = JSON.parse(body);
-        name = bodyObj.first_name;
-        greeting = "Hi " + name + ". ";
-      }
-      var message = greeting + "You da best!";
-      sendMessage(senderId, {text: message});
-    });
-  }
-}
-
-// sends message to user
-function sendMessage(recipientId, message) {
-  request({
-    url: "https://graph.facebook.com/v2.6/me/messages",
-    qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-    method: "POST",
-    json: {
-      recipient: {id: recipientId},
-      message: message,
-    }
-  }, function(error, response, body) {
-    if (error) {
-      console.log("Error sending message: " + response.error);
-    }
-  });
+  
+function receivedMessage(event) {
+  // Putting a stub for now, we'll expand it in the following steps
+  console.log("Message data: ", event.message);
 }
