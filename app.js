7a3d8be774070a83ea24bcef13627889c2d57f4b
Jake Reid <jake@Jakes-MacBook-Pro.local>
2017-04-21 17:54:13 +0100

    The cookie fix worked, now just wrinkling out some problems

--- app.js
+++ app.js
@@ -122,7 +122,7 @@ function loginFirefly(eventData) {
     jar: myjar,
     followAllRedirects: true,
     method: 'POST',
-    form: AUTH[eventData.recipientID]
+    form: AUTH[eventData.senderID]
   }, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       getTasks(body, eventData)
